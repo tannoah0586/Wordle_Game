810b67f4563caa228eb334b22a265d4b20888fec
tannoah <tannoah@me.com>
2024-11-01 15:02:47 +0800

    changed updateClick() to remove the resetting of board array to put it in backspace()

--- app.js
+++ app.js
@@ -26,21 +26,27 @@ const handleClick = (event) => {
         handleEnterPress();  
         isEnterPressed = true;                                           //important Enter keydown function...
     } else if (event.key === 'Backspace') {
-        if (!isEnterPressed && rowNumber === 0) {                                           //locks in currentWord and prevents backspace() once 5 letters and enter is pressed
+        if (!isEnterPressed && rowNumber === 0) {                        //locks in currentWord and prevents backspace() once 5 letters and enter is pressed
             handleBackspacePress();  
         }                                       
     } else if (event.key.length === 1 && event.key.match(/[a-zA-Z]/)) { //https://stackoverflow.com/questions/38955573/how-to-check-keyboardevent-key-in-specific-range-in-javascript + https://stackoverflow.com/questions/12745930/javascript-regex-uppercase-and-lowercase-and-mixed
         currentWord += event.key;
     }
-    board = [                                                              // need to clear the board array inorder to update the new currentWord.
-        '', '', '', '', '',
-        '', '', '', '', '',
-    ];                     
-    
+    // board = [                                                              // need to clear the board array inorder to update the new currentWord.
+    //     '', '', '', '', '',
+    //     '', '', '', '', '',
+    // ];                     
+    if (currentWord.length > 0) {
+        startingIndex = rowNumber * 5;
+        for (let i = 0; i < currentWord.length; i++) {                      // Update the board array with 'new' currentWord
+            board[i+startingIndex] = currentWord[i];
+        };
+        console.log(board)
+    };
 
-    for (let i = 0; i < currentWord.length; i++) {                      // Update the board array with 'new' currentWord
-        board[i] = currentWord[i];
-    }
+
+    // console.log(`current word length: ${currentWord.length}`)
+    // console.log(`starting index: ${startingIndex}`)
     render();
 };
 
@@ -55,11 +61,14 @@ const handleEnterPress = () => {
             } else {
                 winner = true;
                 }
+                    console.log(`board: ${board}`)
     }; 
 };
 
 const handleBackspacePress = () => {
-    currentWord = currentWord.slice(rowNumber*5, -1);                             //slice() works...
+    currentWord = currentWord.slice(0, -1);                             //slice() works...
+    const startingIndex = rowNumber *5;
+    board[startingIndex+currentWord.length] = '';
 }
 
 const getSameLetters = (targetWord,currentWord) => {
@@ -80,7 +89,7 @@ const updateGeeen = (sameCorrectLocation) => {
         if(sameCorrectLocation.includes(i)) {
             document.getElementById(guessEl[startingIndex + i].id).style.backgroundColor = 'rgb(144, 238, 144)';  
         }
-    } 
+    }  
 };
 
 const updateYellow = (sameButDifferentLocation) => {
@@ -89,22 +98,21 @@ const updateYellow = (sameButDifferentLocation) => {
         if(sameButDifferentLocation.includes(i)) {
             document.getElementById(guessEl[startingIndex + i].id).style.backgroundColor = 'rgb(255, 255, 0)'; 
         }
-    } 
+    }  
 };
 
 const updateBoard = () => {
     startingIndex = rowNumber * 5;
     board.forEach((element,index) => {
-        
-        if(index + startingIndex < guessEl.length) {
-            guessEl[index+startingIndex].innerText = element;
+        if(index < guessEl.length) {                                //restriction condition to prevent undefined guessEl.innertext
+            guessEl[index].innerText = element;
         };
     });
 };
 
 const updateMessage = () => {
     if(winner === true) {
-        messageEL.textContent = `You guess correctly!`;
+        messageEL.textContent = `You guess the word correctly!`;
     };
 };
 
