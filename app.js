492e3927c99fbf4fbfb679fe9406f65c393733f0
tannoah <tannoah@me.com>
2024-11-02 14:27:48 +0800

    found the bugs within the updateGreen and updateYellow(). game now functions as per design

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*-------------------------------- Constants --------------------------------*/
 const wordLength = 5;
-const maxAttempt = 2;
-const targetWord = 'oklah';
+const maxAttempt = 6;
+const targetWord = 'hello';
 /*---------------------------- Variables (state) ----------------------------*/
     // let guesses = [];
     let currentWord = '';
@@ -15,6 +15,7 @@ const targetWord = 'oklah';
         '','','','','',
     ];
     let rowNumber = 0;
+    let attepmt = 0;
 /*------------------------ Cached Element References ------------------------*/
 
 const guessEl = document.querySelectorAll('.guesses');
@@ -54,12 +55,13 @@ const handleEnterPress = () => {
                 getSameLetters(targetWord,currentWord);                 //compare target and current word function
                 updateGeeen(sameCorrectLocation);                       //arrays are passed to the fuctions to avoid using global arrays which contributes to code smell
                 updateYellow(sameButDifferentLocation);                 //arrays are passed to the fuctions to avoid using global arrays which contributes to code smell  
-                rowNumber +=1;              
-                currentWord = '';                                       //resets currentWord for new line
+                rowNumber += 1;   
+                attepmt +=1;           
+                currentWord = '';                                       //resets currentWord for new row
             } else {
                 winner = true;
                 }
-                    console.log(`board: ${board}`)
+                    console.log(`board array: ${board}`)
     }; 
 };
 
@@ -69,25 +71,24 @@ const handleBackspacePress = () => {
     board[startingIndex + currentWord.length] = '';
 }
 
-const getSameLetters = (targetWord,currentWord) => {                        //aim for this function is to build the 2 arrays to update board
+const getSameLetters = (targetWord,currentWord) => {                                    //this function is to build the 2 arrays to update board with color codes
     const startingIndex = rowNumber * 5;  
-    for (let i = 0; i < targetWord.length; i++) {                           //testing if board.length should be the correct array as targetword.length is only 5 letters long... so it runs out of space fast
-        if(targetWord[i] === currentWord[i+startingIndex]) {
-            sameCorrectLocation.push(i+startingIndex);                            //https://stackoverflow.com/questions/70040227/how-do-you-check-two-strings-in-js-and-determine-if-any-letters-in-each-is-place
-        } else if (targetWord.includes(currentWord[i+startingIndex]) && targetWord[i] !== currentWord[i+startingIndex]) {
-            sameButDifferentLocation.push(i+startingIndex);
+    for (let i = 0; i < targetWord.length; i++) {
+        if(targetWord[i] === currentWord[i]) {
+            sameCorrectLocation.push(i + startingIndex);                                //https://stackoverflow.com/questions/70040227/how-do-you-check-two-strings-in-js-and-determine-if-any-letters-in-each-is-place
+        } else if (targetWord.includes(currentWord[i]) && targetWord[i] !== currentWord[i]) {                       
+            sameButDifferentLocation.push(i + startingIndex);
         }
-        console.log(`correct letter but different location: ${sameButDifferentLocation}`);                             //array is correct but buggy... as it accounts for only 5 elements
-        console.log(`correct letter correct location: ${sameCorrectLocation}`);                                  //array is correct but buggy... as it accounts for only 5 elements
     }
-    return;
+    console.log(`correct letter but different location: ${sameButDifferentLocation}`);                               //array push is buggy...
+    console.log(`correct letter correct location: ${sameCorrectLocation}`);                                          //array is okay 
 }
 
 const updateGeeen = (sameCorrectLocation) => {
     const startingIndex = rowNumber * 5;
     for (let i = 0; i < guessEl.length; i++) {
         if(sameCorrectLocation.includes(i)) {
-            document.getElementById(guessEl[startingIndex + i].id).style.backgroundColor = 'rgb(144, 238, 144)';  
+            document.getElementById(guessEl[i].id).style.backgroundColor = 'rgb(144, 238, 144)';  
         }
     }  
 };
@@ -96,7 +97,7 @@ const updateYellow = (sameButDifferentLocation) => {
     const startingIndex = rowNumber * 5;
     for (let i = 0; i < guessEl.length; i++) {
         if(sameButDifferentLocation.includes(i)) {
-            document.getElementById(guessEl[startingIndex + i].id).style.backgroundColor = 'rgb(255, 255, 0)'; 
+            document.getElementById(guessEl[i].id).style.backgroundColor = 'rgb(255, 255, 0)'; 
         }
     }  
 };
@@ -113,6 +114,9 @@ const updateBoard = () => {
 const updateMessage = () => {
     if(winner === true) {
         messageEL.textContent = `You guess the word correctly!`;
+    };
+    if (attepmt === maxAttempt && winner === false) {
+        messageEL.textContent = 'You lose, try harder next time!'
     };
 };
 
